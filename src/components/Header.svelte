82ba5a09367fc0075606e01f4b6394b28b45c452
mantra raisi <mantra.raisi@gmail.com>
2024-02-29 14:41:30 +0100

    add new page and implement main page

---
+++ src/components/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<header class="flex flex-col relative z-20">
+  <div
+    class="max-w-[1400px] mx-auto w-full flex item-center justify-between p-4 py-6"
+  >
+    <a href="/">
+      <h1 class="font-semibold">Bas<span class="text-indigo-400">ket</span></h1>
+    </a>
+
+    <button class="md:hidden grid place-items-center">
+      <i class="fa-solid fa-bars"></i>
+    </button>
+    <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+      <a
+        href="#product"
+        class="duration-200 hover:text-indigo-400 cursor-pointer">product</a
+      >
+      <a
+        href="#reviews"
+        class="duration-200 hover:text-indigo-400 cursor-pointer">reviews</a
+      >
+      <a href="#faqs" class="duration-200 hover:text-indigo-400 cursor-pointer"
+        >Faq's</a
+      >
+      <button class="specialBtn">
+        <p>Start free today</p>
+      </button>
+    </nav>
+  </div>
+</header>
---
+++ src/components/Hero.svelte
@@ -0,0 +1,47 @@
+<script>
+  import CtAs from "./CTAs.svelte";
+  import Header from "./Header.svelte";
+  import SectionWrapper from "./SectionWrapper.svelte";
+</script>
+
+<SectionWrapper id>
+  <Header />
+  <div
+    class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14"
+  >
+    <!-- svelte-ignore a11y-missing-content -->
+    <h2
+      class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center"
+    >
+      <span class="text-indigo-400"> Start </span> your shopping
+      <span class="text-slate-600 line-through">journey </span><br />with
+      <span class="text-indigo-400">us</span>
+    </h2>
+    <p
+      class="text-xl sm:text-2xl md:text-3xl
+    text-center max-w-[1000px] mx-auto w-full"
+    >
+      less thinking more buying.we're the market in your pocket full of <span
+        class="italic"
+      >
+        products that you need and you don't
+      </span>and much more
+    </p>
+    <CtAs />
+    <div class="flex items-center justify-center gap-2 text-base">
+      <p>4.6</p>
+
+      {#each [0, 1, 2, 3, 4] as index}
+        <div class="grid place-item-center relative">
+          <i class="fa-solid fa-star opacity-0"></i>
+          <div
+            class={"absolute top-0 left-0 grid place-items-center" +
+              (index === 4 ? "w-[60%] overflow-hidden" : "")}
+          >
+            <i class="fa-solid fa-star text-amber-400"></i>
+          </div>
+        </div>
+      {/each}
+    </div>
+  </div>
+</SectionWrapper>
